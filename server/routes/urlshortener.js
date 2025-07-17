85f125913d1241d0869bb1bcc566ce6ca355cff8
Ali Imam Hridoy <hridoy15-14667@diu.edu.bd>
2025-07-17 12:50:06 +0600

    ok

--- server/routes/urlshortener.js
+++ server/routes/urlshortener.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ShortUrl = require('../models server/ShortUrl'); 
+const ShortUrl = require('../models/ShortUrl'); 
 const { nanoid } = require('nanoid');
 
 // POST - Create short URL
