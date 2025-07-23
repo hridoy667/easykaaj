449fa2bf2eb491c95054b0bc4355bb82fa149d87
Ali Imam Hridoy <hridoy15-14667@diu.edu.bd>
2025-07-23 13:26:29 +0600

    ok

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,27 @@
-// main.jsx
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
+export function Root() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        for (const registration of registrations) {
+          registration.unregister()
+        }
+      })
+    }
+  }, [])
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<Root />)
